28348c55e84e391743266dc78f1c0063516774a1
Mateusz <187099271+5moothie@users.noreply.github.com>
2026-06-09 11:52:11 +0200

    added shoe tests

--- include/blackjack/shoe.hpp
+++ include/blackjack/shoe.hpp
@@ -8,6 +8,8 @@
 Shoe needs to be reshuffled so it's impossible for it to get empty on hand
 
 It is automatically filled and shuffled on initialization.
+
+It is up to the user to set correct cutCard and reshuffle the deck. If there are no cards in the deck, getCard will throw std::runtime_error();
 */
 
 class Shoe {
@@ -21,11 +23,12 @@ private:
   void clear() noexcept;
 
 public:
-  Shoe(size_t numberOfDecks, float cutCard);
+  Shoe(size_t numberOfDecks, float cutCard); 
 
   [[nodiscard]] Card getCard();
 
   void reshuffle() noexcept;
 
+  // if there is <= cutCard decks left in the shoe - needs reshuffle will be true
   bool needsReshuffle() const noexcept;
 };
