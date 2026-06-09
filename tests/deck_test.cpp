b0d6e9c2edac766493ec40838a9a27fef7ddcc5e
Mateusz <187099271+5moothie@users.noreply.github.com>
2026-06-09 11:39:22 +0200

    added deck tests

---
+++ tests/deck_test.cpp
@@ -0,0 +1,58 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include "card/deck.hpp"
+#include <set>
+
+TEST_CASE("Deck contains 52 cards")
+{
+  Deck deck;
+
+  auto cards = std::move(deck).releaseCards();
+
+  REQUIRE(cards.size() == 52);
+}
+
+TEST_CASE("Deck contains every card exactly once")
+{
+  Deck deck;
+  auto cards = std::move(deck).releaseCards();
+
+  std::set<std::pair<Rank, Suit>> uniqueCards;
+
+  for(const auto& card : cards)
+  {
+    uniqueCards.insert({
+      card.getRank(),
+      card.getSuit()
+    });
+  }
+
+  REQUIRE(uniqueCards.size() == 52);
+}
+
+TEST_CASE("Deck contains 13 cards of each suit")
+{
+  Deck deck;
+  auto cards = std::move(deck).releaseCards();
+
+  int hearts = 0;
+  int diamonds = 0;
+  int clubs = 0;
+  int spades = 0;
+
+  for(const auto& card : cards)
+  {
+    switch(card.getSuit())
+    {
+      case Suit::Hearts:   hearts++; break;
+      case Suit::Diamonds: diamonds++; break;
+      case Suit::Clubs:    clubs++; break;
+      case Suit::Spades:   spades++; break;
+    }
+  }
+
+  REQUIRE(hearts == 13);
+  REQUIRE(diamonds == 13);
+  REQUIRE(clubs == 13);
+  REQUIRE(spades == 13);
+}
